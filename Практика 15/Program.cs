6cd0d425cf148a77ffc7e1b81040c90f43fd4634
ACER <ACER@DESKTOP-O7A416V>
2022-12-13 17:41:43 +0300

    Добавьте файлы проекта.

---
+++ Практика 15/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Задача__1
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            {
+                Console.Write("Введите строку: ");
+                string str = Console.ReadLine();
+                int i = 0;
+                int j = 1;
+                var index = new List<int>();
+                foreach (Match m in Regex.Matches(str, @"red?"))
+                {
+                    index.Add(j);
+                    i++;
+                    j++;
+                }
+                Console.WriteLine("Количество слов в тексте: {0}", index.Count);
+            }
+        }
+    }
+}
